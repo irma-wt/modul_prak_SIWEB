5afb75aa1ccce34201798dc9506c0a7ed3c15fa2
Irmawati <irmawati@mhs.itenas.ac.id>
2026-04-02 12:03:49 +0700

    Add product and category management features with database seeding and UI updates

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 // Halaman Utama
 Route::get('/', function () {
@@ -15,4 +16,8 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.proses');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-?>
+// Halaman produk
+Route::get('/products', [ProductController::class, 'index'])->name('products');
+
+# Code Route untuk Menambah Product Baru
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
